f7d5a8572ea88dd9f31909d932e7fb07fc10b225
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-03-27 12:11:01 -0500

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.scss";
+// Routes
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages
+import { Home, MealDetails, Error, Category } from "./pages/index";
+
+import Header from "./Components/Header/Header";
+import SideBar from "./Components/SideBar/SideBar";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hola mundo!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <SideBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/meal/:id" element={<MealDetails />} />
+        <Route path="/meal/category/:name" element={<Category />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
