bf24553ee1d8a7e783d55d5599335931d0ecef87
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-03-27 13:03:24 -0500

    sideBar named in context

--- src/context/sideBarContext.jsx
+++ src/context/sideBarContext.jsx
@@ -1,38 +1,37 @@
-import React, {createContext, useContext, useReducer} from "react";
+import React, { createContext, useContext, useReducer } from "react";
 import reducer from "../reducers/sidebarReducer";
-import {
-    OPEN_SIDEBAR,
-    CLOSE_SIDEBAR
-} from "../actions/actions";
+import { OPEN_SIDEBAR, CLOSE_SIDEBAR } from "../actions/actions";
 
 const initialState = {
-    isSidebarOpen: false
-}
+  isSidebarOpen: false,
+};
 
-const SidebarContext = createContext({});
+const SideBarContext = createContext({});
 
-export const SidebarProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+export const SidebarProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const openSidebar = () => {
-        dispatch({type: OPEN_SIDEBAR});
-    }
+  const openSidebar = () => {
+    dispatch({ type: OPEN_SIDEBAR });
+  };
 
-    const closeSidebar = () => {
-        dispatch({ type: CLOSE_SIDEBAR });
-    }
+  const closeSidebar = () => {
+    dispatch({ type: CLOSE_SIDEBAR });
+  };
 
-    return (
-        <SidebarContext.Provider value = {{
-            ...state,
-            openSidebar, 
-            closeSidebar
-        }}>
-            {children}
-        </SidebarContext.Provider>
-    )
-}
+  return (
+    <SideBarContext.Provider
+      value={{
+        ...state,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </SideBarContext.Provider>
+  );
+};
 
-export const useSidebarContext = () => {
-    return useContext(SidebarContext);
-}
+export const useSideBarContext = () => {
+  return useContext(SideBarContext);
+};
